59d3d8b0855ca91e8e94147320d9651fcf0eba4d
Nabhya-yadav <nabhya.yadav20@gmail.com>
2025-10-10 14:54:12 +0530

    Add files via upload

---
+++ Let's begin with loops/Loops/reversing number.py
@@ -0,0 +1,6 @@
+n=int(input("Enter the number range to be reversed"))
+
+
+for i in range(n,0,-1):
+    print(i)
+    
---
+++ Let's begin with loops/Loops/reversing string.py
@@ -0,0 +1,8 @@
+str1=input("Enter your string to be reversed:")
+str2=""
+
+for i in str1:
+    str2=i+str2
+
+print(str1)
+print(str2)
