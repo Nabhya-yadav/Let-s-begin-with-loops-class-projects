f28a9f22c6efc8ff79d8e031b47e0104c6918369
Nabhya-yadav <nabhya.yadav20@gmail.com>
2025-10-24 12:45:46 +0530

    Add files via upload

---
+++ Patterns/half pyramid triangle.py
@@ -0,0 +1,14 @@
+row=int(input("Enter the number of rows  "))
+
+for i in range(row):
+
+    num=1
+
+    for j in range(i+1):
+        print(num,end=" ")
+
+        num=num+1
+
+    print()
+
+    
